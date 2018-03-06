0e4aedb2ec5921db1765fdecc28fed7051186d31
Liz Davies <sarah.e.e.davies@gmail.com>
2018-03-06 14:14:25 -0500

    Most of drop and duplicate done

--- iteration1/src/Main/HostConnectionManager.java
+++ iteration1/src/Main/HostConnectionManager.java
@@ -74,5 +74,113 @@ public class HostConnectionManager extends Thread{
 			System.out.print("HostThread: ");
 			e.printStackTrace();
 		}
-	}			
+	}		
+	public void runWithPacketDropError(int packetNum) {
+		try {
+			datagramOut = new DatagramPacket(originalData,originalLength,InetAddress.getLocalHost(),69);
+			System.out.println("HostThread: Client -> Server");
+			socket.send(datagramOut);
+			data = new byte[516];
+			datagramIn = new DatagramPacket(data,516);
+			socket.receive(datagramIn);
+			serverAddress = datagramIn.getSocketAddress();
+			int currentPacket = 1;
+			while(true) {
+				if(packetNum != currentPacket) {
+					//ONLY SEND PACKET IF IT DOES NOT EQUAL THE LOSSY PACKET
+					if(datagramIn.getSocketAddress().equals(clientAddress)) {
+						datagramOut = new DatagramPacket(datagramIn.getData(),datagramIn.getLength(),serverAddress);
+						System.out.println("HostThread: Client -> Server");
+					}else if(datagramIn.getSocketAddress().equals(serverAddress)){
+						datagramOut = new DatagramPacket(datagramIn.getData(),datagramIn.getLength(),clientAddress);
+						System.out.println("HostThread: Server -> Client");
+					}else {
+						System.out.println("HostThread: Interlopping connection");
+						System.out.println("HostThread: Server address: "+serverAddress);
+						System.out.println("HostThread: Client address: "+clientAddress);
+						System.out.println("HostThread: New address: "+datagramIn.getSocketAddress());
+						break;
+					}
+					socket.send(datagramOut);
+					data = new byte[516];
+					datagramIn = new DatagramPacket(data,516);
+				}
+				socket.receive(datagramIn);
+				++currentPacket;
+			}			
+		}catch(SocketTimeoutException e) {
+			System.out.println("HostThread: Socket timed out. Closing socket");
+			socket.close();
+			System.exit(0);
+		}
+		catch(SocketException e) {
+			System.out.println("HostThread: "+e.getCause());
+			socket.close();
+			System.exit(0);
+		} catch (IOException e) {
+			System.out.print("HostThread: ");
+			e.printStackTrace();
+		}
+	}		
+	public void runWithPacketDuplicationError(int packetNum, int DelayMS) {
+		try {
+			datagramOut = new DatagramPacket(originalData,originalLength,InetAddress.getLocalHost(),69);
+			System.out.println("HostThread: Client -> Server");
+			socket.send(datagramOut);
+			data = new byte[516];
+			datagramIn = new DatagramPacket(data,516);
+			socket.receive(datagramIn);
+			serverAddress = datagramIn.getSocketAddress();
+			int currentPacket = 1;
+			while(true) {
+				
+				if(datagramIn.getSocketAddress().equals(clientAddress)) {
+					datagramOut = new DatagramPacket(datagramIn.getData(),datagramIn.getLength(),serverAddress);
+					System.out.println("HostThread: Client -> Server");
+					if(currentPacket == packetNum) delayPacket(datagramIn,DelayMS,serverAddress);
+				}else if(datagramIn.getSocketAddress().equals(serverAddress)){
+					datagramOut = new DatagramPacket(datagramIn.getData(),datagramIn.getLength(),clientAddress);
+					System.out.println("HostThread: Server -> Client");
+					//setDelay function
+					if(currentPacket == packetNum) delayPacket(datagramIn,DelayMS,clientAddress);
+				}else {
+					System.out.println("HostThread: Interlopping connection");
+					System.out.println("HostThread: Server address: "+serverAddress);
+					System.out.println("HostThread: Client address: "+clientAddress);
+					System.out.println("HostThread: New address: "+datagramIn.getSocketAddress());
+					break;
+				}
+				socket.send(datagramOut);
+				data = new byte[516];
+				datagramIn = new DatagramPacket(data,516);
+				socket.receive(datagramIn);
+				++currentPacket;
+			}			
+		}catch(SocketTimeoutException e) {
+			System.out.println("HostThread: Socket timed out. Closing socket");
+			socket.close();
+			System.exit(0);
+		}
+		catch(SocketException e) {
+			System.out.println("HostThread: "+e.getCause());
+			socket.close();
+			System.exit(0);
+		} catch (IOException e) {
+			System.out.print("HostThread: ");
+			e.printStackTrace();
+		}
+	}
+	public void delayPacket(DatagramPacket pack,int delay,SocketAddress addr) {
+		//setDelay function
+		DatagramPacket tempDatagram = DatagramPacket(pack.getData(),pack.getLength(),addr);
+		new java.util.Timer().schedule( 
+		        new java.util.TimerTask() {
+		            @Override
+		            public void run() {
+		            	socket.send(tempDatagram);
+		            }
+		        }, 
+		        delay 
+		);
+	}
 }
