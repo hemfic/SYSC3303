da0f3654403ff545fefba3cb9b2ce3f0a3ab9443
hemfic <ericreesor@outlook.com>
2018-03-08 17:18:44 -0500

    added console and delay error code

--- iteration1/src/Main/Host.java
+++ iteration1/src/Main/Host.java
@@ -4,6 +4,7 @@ import java.io.*;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.SocketException;
+import java.util.Scanner;
 
 public class Host {
 	DatagramPacket sendPacket, receivePacket;
@@ -23,12 +24,12 @@ public class Host {
 	         System.exit(1);
 		} 
 	}
-	public void receive() {
+	public void receive(int mode, int packetNumber,int delayTime) {
 		try {
 			System.out.println("Host: Waiting for package...");
 			sockR.receive(receivePacket);
 			System.out.println("Host: Package received");
-	        (new Thread(new HostConnectionManager(receivePacket))).start();
+	        (new Thread(new HostConnectionManager(receivePacket,mode,packetNumber,delayTime))).start();
 		}catch(IOException e) {
 			System.out.println("Host: Fatal error");
 			sockR.close();
@@ -37,10 +38,71 @@ public class Host {
 		}
 		
 	}
+	public void receiveWithLoss() {
+		Scanner s= new Scanner(System.in);
+		String input;
+		System.out.println("Enter what packet you want to lose. (0=Request packet, 1=1st transfer packet...) \nIt is assumed you choose a packet number knowing how many packets there will be.\nDo not choose packet 8 if there will only be 7 packets.");
+		input=s.nextLine();
+		receive(1,Integer.parseInt(input),0);
+		s.close();
+	}
+	public void receiveWithDelay() {
+		Scanner s= new Scanner(System.in);
+		String input;
+		int packetNum;
+		System.out.println("Enter what packet you want to delay. (0=Request packet, 1=1st transfer packet...) \nIt is assumed you choose a packet number knowing how many packets there will be.\nDo not choose packet 8 if there will only be 7 packets.");
+		input=s.nextLine();
+		packetNum=Integer.parseInt(input);
+		System.out.println("Enter the delay for the chosen packet in milliseconds");
+		input=s.nextLine();
+		receive(2,packetNum,Integer.parseInt(input));
+		s.close();
+	}
+	public void receiveWithDupe() {
+		Scanner s= new Scanner(System.in);
+		String input;
+		int packetNum;
+		System.out.println("Enter what packet you want to duplicate. (0=Request packet, 1=1st transfer packet...) \nIt is assumed you choose a packet number knowing how many packets there will be.\nDo not choose packet 8 if there will only be 7 packets.");
+		input=s.nextLine();
+		packetNum=Integer.parseInt(input);
+		System.out.println("Enter the delay for the duplicated copy of the packet in milliseconds");
+		input=s.nextLine();
+		receive(3,packetNum,Integer.parseInt(input));
+		s.close();
+	}
 	public static void main(String[] args) {
 		Host h=new Host();
-		while(true) {
-			h.receive();
+		boolean run=true;
+		Scanner s= new Scanner(System.in);
+		String input;
+		while(run) {
+			System.out.println("Choose simulation for next transfer request:");
+			System.out.println("1) Normal Mode");
+			System.out.println("2) Lose Packet");
+			System.out.println("3) Delay Packet");
+			System.out.println("4) Duplicate Packet");
+			System.out.println("5) EXIT");
+			input=s.nextLine();
+	    	if(Integer.parseInt(input)==1) {
+	    		System.out.println("Running Normal Mode");
+	    		h.receive(0,0,0);
+	    	}else if(Integer.parseInt(input)==2) {
+	    		System.out.println("Running Packet Loss Mode");
+	    		h.receiveWithLoss();
+	    	}else if(Integer.parseInt(input)==3) {
+	    		System.out.println("Running Packet Delay Mode");
+	    		h.receiveWithDelay();
+	    	}else if(Integer.parseInt(input)==4) {
+	    		System.out.println("Running Packet Duplication Mode");
+	    		h.receiveWithDupe();
+	    	}else if(Integer.parseInt(input)==5) {
+	    		System.out.println("Exiting");
+	    		run=false;
+	    	}else {
+	    		System.out.println("Invalid input. Please Enter Again");
+	    	}
+			
 		}
+		s.close();
 	}
 }
--- iteration1/src/Main/HostConnectionManager.java
+++ iteration1/src/Main/HostConnectionManager.java
@@ -8,10 +8,13 @@ public class HostConnectionManager extends Thread{
 	DatagramPacket datagramIn,datagramOut,originalRequest;
 	SocketAddress clientAddress, serverAddress;
 	byte[] data,originalData;
-	int originalLength;
+	int originalLength,mode,packetNumber,delayTime;
 	
-	public HostConnectionManager(DatagramPacket request) {
+	public HostConnectionManager(DatagramPacket request, int modeNum, int packetNum, int delayT) {
 		System.out.println("HostThread: created");
+		this.mode = modeNum;
+		this.packetNumber=packetNum;
+		this.delayTime=delayT;
 		originalRequest = request;
 		originalData = request.getData();
 		originalLength = request.getLength();
@@ -34,46 +37,55 @@ public class HostConnectionManager extends Thread{
 		}
 	}
 	public void run() {
-		try {
-			datagramOut = new DatagramPacket(originalData,originalLength,InetAddress.getLocalHost(),69);
-			System.out.println("HostThread: Client -> Server");
-			socket.send(datagramOut);
-			data = new byte[516];
-			datagramIn = new DatagramPacket(data,516);
-			socket.receive(datagramIn);
-			serverAddress = datagramIn.getSocketAddress();
-			while(true) {
-				if(datagramIn.getSocketAddress().equals(clientAddress)) {
-					datagramOut = new DatagramPacket(datagramIn.getData(),datagramIn.getLength(),serverAddress);
-					System.out.println("HostThread: Client -> Server");
-				}else if(datagramIn.getSocketAddress().equals(serverAddress)){
-					datagramOut = new DatagramPacket(datagramIn.getData(),datagramIn.getLength(),clientAddress);
-					System.out.println("HostThread: Server -> Client");
-				}else {
-					System.out.println("HostThread: Interlopping connection");
-					System.out.println("HostThread: Server address: "+serverAddress);
-					System.out.println("HostThread: Client address: "+clientAddress);
-					System.out.println("HostThread: New address: "+datagramIn.getSocketAddress());
-					break;
-				}
+		if(mode==0) {
+			try {
+				datagramOut = new DatagramPacket(originalData,originalLength,InetAddress.getLocalHost(),69);
+				System.out.println("HostThread: Client -> Server");
 				socket.send(datagramOut);
 				data = new byte[516];
 				datagramIn = new DatagramPacket(data,516);
 				socket.receive(datagramIn);
-			}			
-		}catch(SocketTimeoutException e) {
-			System.out.println("HostThread: Socket timed out. Closing socket");
-			socket.close();
-			System.exit(0);
+				serverAddress = datagramIn.getSocketAddress();
+				while(true) {
+					if(datagramIn.getSocketAddress().equals(clientAddress)) {
+						datagramOut = new DatagramPacket(datagramIn.getData(),datagramIn.getLength(),serverAddress);
+						System.out.println("HostThread: Client -> Server");
+					}else if(datagramIn.getSocketAddress().equals(serverAddress)){
+						datagramOut = new DatagramPacket(datagramIn.getData(),datagramIn.getLength(),clientAddress);
+						System.out.println("HostThread: Server -> Client");
+					}else {
+						System.out.println("HostThread: Interlopping connection");
+						System.out.println("HostThread: Server address: "+serverAddress);
+						System.out.println("HostThread: Client address: "+clientAddress);
+						System.out.println("HostThread: New address: "+datagramIn.getSocketAddress());
+						break;
+					}
+					socket.send(datagramOut);
+					data = new byte[516];
+					datagramIn = new DatagramPacket(data,516);
+					socket.receive(datagramIn);
+				}			
+			}catch(SocketTimeoutException e) {
+				System.out.println("HostThread: Socket timed out. Closing socket");
+				socket.close();
+				System.exit(0);
+			}
+			catch(SocketException e) {
+				System.out.println("HostThread: "+e.getCause());
+				socket.close();
+				System.exit(0);
+			} catch (IOException e) {
+				System.out.print("HostThread: ");
+				e.printStackTrace();
+			}
+		}else if(mode==1) {
+			runWithPacketDropError(packetNumber);
+		}else if(mode==2) {
+			runWithPacketDelayError(packetNumber,delayTime);
+		}else if(mode==3) {
+			runWithPacketDuplicationError(packetNumber,delayTime);
 		}
-		catch(SocketException e) {
-			System.out.println("HostThread: "+e.getCause());
-			socket.close();
-			System.exit(0);
-		} catch (IOException e) {
-			System.out.print("HostThread: ");
-			e.printStackTrace();
-		}
+		
 	}		
 	public void runWithPacketDropError(int packetNum) {
 		try {
@@ -144,7 +156,7 @@ public class HostConnectionManager extends Thread{
 					//setDelay function
 					if(currentPacket == packetNum) delayPacket(datagramIn,DelayMS,clientAddress);
 				}else {
-					System.out.println("HostThread: Interlopping connection");
+					System.out.println("HostThread: Interlooping connection");
 					System.out.println("HostThread: Server address: "+serverAddress);
 					System.out.println("HostThread: Client address: "+clientAddress);
 					System.out.println("HostThread: New address: "+datagramIn.getSocketAddress());
@@ -172,15 +184,67 @@ public class HostConnectionManager extends Thread{
 	}
 	public void delayPacket(DatagramPacket pack,int delay,SocketAddress addr) {
 		//setDelay function
-		DatagramPacket tempDatagram = DatagramPacket(pack.getData(),pack.getLength(),addr);
+		DatagramPacket tempDatagram = new DatagramPacket(pack.getData(),pack.getLength(),addr);
 		new java.util.Timer().schedule( 
 		        new java.util.TimerTask() {
 		            @Override
 		            public void run() {
-		            	socket.send(tempDatagram);
+		            	try {
+							socket.send(tempDatagram);
+						} catch (IOException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
 		            }
 		        }, 
 		        delay 
 		);
+	}
+	
+	public void runWithPacketDelayError(int packetNum, int delayMS) {
+		int packetCount = 0;
+		try {
+			datagramOut = new DatagramPacket(originalData,originalLength,InetAddress.getLocalHost(),69);
+			System.out.println("HostThread: Client -> Server");
+			socket.send(datagramOut);
+			data = new byte[516];
+			datagramIn = new DatagramPacket(data,516);
+			socket.receive(datagramIn);
+			packetCount+=1;
+			serverAddress = datagramIn.getSocketAddress();
+			while(true) {
+				if(datagramIn.getSocketAddress().equals(clientAddress)) {
+					datagramOut = new DatagramPacket(datagramIn.getData(),datagramIn.getLength(),serverAddress);
+					System.out.println("HostThread: Client -> Server");
+				}else if(datagramIn.getSocketAddress().equals(serverAddress)){
+					datagramOut = new DatagramPacket(datagramIn.getData(),datagramIn.getLength(),clientAddress);
+					System.out.println("HostThread: Server -> Client");
+				}else {
+					System.out.println("HostThread: Interlopping connection");
+					System.out.println("HostThread: Server address: "+serverAddress);
+					System.out.println("HostThread: Client address: "+clientAddress);
+					System.out.println("HostThread: New address: "+datagramIn.getSocketAddress());
+					break;
+				}
+				if(packetCount==packetNum)delayPacket(datagramIn,delayMS,clientAddress);
+				else {socket.send(datagramOut);}
+				data = new byte[516];
+				datagramIn = new DatagramPacket(data,516);
+				socket.receive(datagramIn);
+				packetCount+=1;
+			}			
+		}catch(SocketTimeoutException e) {
+			System.out.println("HostThread: Socket timed out. Closing socket");
+			socket.close();
+			System.exit(0);
+		}
+		catch(SocketException e) {
+			System.out.println("HostThread: "+e.getCause());
+			socket.close();
+			System.exit(0);
+		} catch (IOException e) {
+			System.out.print("HostThread: ");
+			e.printStackTrace();
+		}
 	}
 }
